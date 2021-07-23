4568f2366a6cc8919bf62dba936da7fe912a5a08
alvaroescobar97 <alvaro325@hotmail.com>
2021-07-23 11:07:18 -0500

    Create dispatch feature working well

--- src/app/feature/envio/components/crear-envio/crear-envio.component.ts
+++ src/app/feature/envio/components/crear-envio/crear-envio.component.ts
@@ -1,3 +1,4 @@
+import { formatDate } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { EnvioService } from '../../shared/service/envio.service';
@@ -20,18 +21,24 @@ export class CrearEnvioComponent implements OnInit {
   }
 
   crear(){
-    console.log(this.envioForm.value);
-    this.envioService.guardar(this.envioForm.value);
+    let body = {...this.envioForm.value, fecha: `${formatDate(
+      this.envioForm.get("fecha")?.value,
+      "yyyy-MM-dd",
+      "es-CO",
+      "UTC"
+    )} 00:00:00` };
+    console.log(body);
+    this.envioService.guardar(body).subscribe(res=> console.log(res),err=> console.log(err));
   }
 
   private construirFormulario(){
     this.envioForm = new FormGroup({
-      cedulaEmisor: new FormControl('', [Validators.required]),
-      cedulaReceptor: new FormControl('', [Validators.required]),
+      cedulaEmisor: new FormControl('', [Validators.required, Validators.maxLength(12)]),
+      cedulaReceptor: new FormControl('', [Validators.required, Validators.maxLength(12)]),
       fecha: new FormControl('', [Validators.required]),
       tipo: new FormControl('', [Validators.required]),
-      peso: new FormControl('', [Validators.required]),
-      valor: new FormControl('', [Validators.required])
+      peso: new FormControl({value:0}, [Validators.required, Validators.min(0)]),
+      valor: new FormControl(0, [Validators.required, Validators.min(0)])
     });
   }
 
--- src/app/feature/envio/shared/service/envio.service.ts
+++ src/app/feature/envio/shared/service/envio.service.ts
@@ -11,12 +11,10 @@ export class EnvioService {
   constructor(protected http: HttpService) {}
 
   public consultar() {
-    return this.http.doGet<Envio[]>(`${environment.endpoint}/envios`, this.http.optsName('consultar envios'));
+    return this.http.doGet<Envio[]>(`${environment.endpoint}/envios`);
   }
 
   public guardar(envio: Envio) {
-    console.log(`${environment.endpoint}/envios`);
-    return this.http.doPost<Envio, boolean>(`${environment.endpoint}/envios`, envio,
-                                                this.http.optsName('crear envio'));
+    return this.http.doPost<Envio, Number>(`${environment.endpoint}/envios`,envio)
   }
 }
