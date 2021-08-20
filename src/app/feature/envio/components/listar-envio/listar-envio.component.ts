9d2b5b63ac1a6662234bbb0707cb7d6e6d9caeca
alvaroescobar97 <alvaro325@hotmail.com>
2021-08-20 15:45:32 -0500

    delete working well with sweetalert2 modal confirm and refresh, also optimized for only one request in list dispatch

--- src/app/feature/envio/components/listar-envio/listar-envio.component.ts
+++ src/app/feature/envio/components/listar-envio/listar-envio.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
 import { Envio } from '../../shared/model/envio';
 import { EnvioService } from '../../shared/service/envio.service';
+import Swal from 'sweetalert2'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-listar-envio',
@@ -10,20 +11,43 @@ import { EnvioService } from '../../shared/service/envio.service';
 })
 export class ListarEnvioComponent implements OnInit {
 
-  public listaEnvios: Observable<Envio[]>;
+  public listaEnvios: Envio[];
 
   displayedColumns: string[] = ['id', 'emisor', 'receptor', 'fecha', 'tipo', 'peso', 'valor', 'action'];
   dataSource: any;
 
-  constructor(protected envioService: EnvioService) {}
+  constructor(protected envioService: EnvioService, private router: Router) {}
 
   ngOnInit(): void {
-    this.listaEnvios = this.envioService.consultar();
+    this.envioService.consultar().subscribe(res=>{
+      this.listaEnvios = res;
+    });
   }
 
   onDelete(id: number){
-    this.envioService.eliminar(id);
-    console.log("deleted "+id);
+    Swal.fire({
+      title: 'Esta seguro que desea eliminar este envio?',
+      text: "Esta accion es irreversible!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, borrar!',
+      cancelButtonText: 'No, cancelar!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.envioService.eliminar(id).subscribe(res => {
+          if(res){
+            Swal.fire(
+              'Envio eliminado!',
+              'El envio ha sido eliminado exitosamente.',
+              'success'
+            );
+            this.router.navigate(['envio/listar']);
+          }
+        });
+      }
+    });
   }
 }
 
